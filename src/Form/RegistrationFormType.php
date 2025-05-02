cfa89c0517311a5decac55b09b08df6f53e937c2
MelissaFrncJrg <melissafranca3@gmail.com>
2025-05-02 12:37:17 +0200

    error handling in registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -10,16 +10,39 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', \Symfony\Component\Form\Extension\Core\Type\EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'register.email.blank',
+                    ]),
+                    new \Symfony\Component\Validator\Constraints\Email([
+                        'message' => 'register.email.invalid',
+                    ]),
+                ],
+                'translation_domain' => 'messages',
+            ])
             ->add('username', TextType::class, [
                 'label' => 'register.username',
                 'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'register.username.blank',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'register.username.too_short',
+                        'max' => 30,
+                    ])
+                ],
+                'translation_domain' => 'messages',
             ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
@@ -33,9 +56,15 @@ class RegistrationFormType extends AbstractType
                         'minMessage' => 'register.password.too_short',
                         'max' => 25,
                     ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/',
+                        'message' => 'register.password.weak'
+                    ]),
+                    new NotCompromisedPassword(([
+                        'message' => 'register.password.compromised'
+                    ]))
                 ],
-            ])
-        ;
+                'translation_domain' => 'messages',]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
